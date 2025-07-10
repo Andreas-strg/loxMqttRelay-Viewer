62a887f8b08f04d2e58c20c7e39250f407d34b8a
Andreas <58007018+Andreas-strg@users.noreply.github.com>
2025-07-10 07:19:46 +0200

    Delete settings.py

--- settings.py
+++
@@ -1,5 +0,0 @@
-# 📡 MQTT Einstellungen
-MQTT_BROKER = os.getenv("MQTT_BROKER", "192.168.178.25")
-MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
-MQTT_USERNAME = os.getenv("MQTT_USERNAME", "andreas")
-MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", "qu!ckMetal90")
